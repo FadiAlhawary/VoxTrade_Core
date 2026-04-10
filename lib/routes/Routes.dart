b0dd4c027fdc7cf1a686ad0a0e8f3b5f0a7709ed
Fadi Alhawary <153363495+FadiAlhawary@users.noreply.github.com>
2026-04-10 21:25:50 +0300

    Revert "added Sign in and sign up pages. updated kotlin to 2.1.0. the 98 modi…"

--- lib/routes/Routes.dart
+++ lib/routes/Routes.dart
@@ -1,18 +1,13 @@
 import 'package:get/get.dart';
 import 'package:voxtrade_core/Components/AppSell/AppShell.dart';
 import 'package:voxtrade_core/pages/Home_page.dart';
-import 'package:voxtrade_core/pages/Sign_In_page.dart';
-import 'package:voxtrade_core/pages/Sign_Up_page.dart';
 import 'package:voxtrade_core/pages/test.dart';
 
 part 'string.dart';
 
 class Routes {
   static List<GetPage> routes = [
-    GetPage(name: RouteStrings.root, page: () => const SignInPage()),
-    GetPage(name: RouteStrings.appShell, page: () => const AppShell()),
-    GetPage(name: RouteStrings.signIn, page: () => const SignInPage()),
-    GetPage(name: RouteStrings.signUp, page: () => const SignUpPage()),
+    GetPage(name: RouteStrings.root, page: () => AppShell()),
     GetPage(name: RouteStrings.home, page: () => HomePage()),
     GetPage(name: RouteStrings.test, page: () => Test()),
   ];
